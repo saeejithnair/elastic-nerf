72f070adf0b907ba726a9bfbfe8fc8b6aec45f52
Saeejith Nair <saeejithn@gmail.com>
2024-02-12 15:21:39 -0500

    Updated .env file path

--- scripts/sweeps/run_wandb_sweep_conda.sh
+++ scripts/sweeps/run_wandb_sweep_conda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Usage:
-# ./src/gen-nerf/scripts/sweep/run_wandb_sweep_conda.sh 0 guoz0t2b [conda_env_name] [wandb_project_name]
+# ./scripts/sweeps/run_wandb_sweep_conda.sh 0 guoz0t2b [conda_env_name] [wandb_project_name]
 
 # Can optionally pass in a conda environment name as the third argument.
 CONDA_ENV_NAME=${3:-elastic_nerf}
@@ -10,7 +10,7 @@ WANDB_PROJ_NAME=${4:-"elastic-nerf"}
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 set -o allexport
-source "${SCRIPT_DIR}/../../../../.env"
+source "${SCRIPT_DIR}/../../.env"
 set +o allexport
 
 # Initialize Conda in bash script
